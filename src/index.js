04579878f66ee6977fc6364087e78d8edec526d6
janeyBee <janebredius@gmail.com>
2024-12-01 16:46:34 +0000

    Added AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function runHaiku(event) {
-  event.preventDefault();
-
+function displayHaiku(response) {
+  console.log("Haiku generated");
   new Typewriter("#haiku", {
-    strings: "What will you do with your one magnificent life?",
+    strings: response.data.answer,
     autoStart: true,
     delay: 55,
     cursor: "",
   });
+}
+
+function runHaiku(event) {
+  event.preventDefault();
+  let instructionInput = document.querySelector("#userInput");
+  let apiKey = "8c03b3b1a26568f726tbe9f7fo7f441d";
+  let prompt = `Generate an English haiku about ${instructionInput.value}.`;
+  let context = `You are a Zen Master. Your mission is to to generate an enlightening haiku in basic html and be sure to put each line of the haiku onto a seperate line. Please do not provide any quotation marks and please do not use a heading. Please do not use the word "whispering" so much. Please sign your haiku inside a <strong> element at the bottom with "Haisei SheCodes AI"`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Haiku");
+  console.log(`prompt is ${prompt}`);
+  console.log(`context is ${context}`);
+
+  axios.get(apiURL).then(displayHaiku);
 }
 
 let haikuformElement = document.querySelector("#haiku_form");
